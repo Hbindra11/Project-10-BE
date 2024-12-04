7142172e5f3536acda736819ed300afaa6329c47
Hema Bindra <hbindra11@yahoo.com>
2024-12-04 19:52:29 +0100

    fixed bugs with autoincrementing id and get now retruns ordered greatest first list if scores

--- controllers/leaderboard.js
+++ controllers/leaderboard.js
@@ -9,13 +9,15 @@ export const createScore = asyncHandler(async (req, res) => {
 
   if (!username || !score)
     throw new ErrorResponse("please provide all required fields", 400);
-  let noOfDocs = await Leaderboard.countDocuments();
-  const newId = noOfDocs + 1; //generating a new id based on number of documents in collection
+  const { _id } = await Leaderboard.findOne({}, {}, { sort: { _id: -1 } });//returns the largest _id so that the next new one can be created
+  //let noOfDocs = await Leaderboard.countDocuments();
+  const newId = _id + 1; //generating a new id based on number of documents in collection but is risky because may get duplicates if a doc is deleted
   const newScore = await Leaderboard.create({ _id: newId, username, score });
   res.status(201).json(newScore);
 });
 
 export const getScores = asyncHandler(async (req, res) => {
-  const scores = await Leaderboard.find();
+  const scores = await Leaderboard.find().sort({ score: -1 }); //returns query result showing scores in sorted order greatest first
+  //console.log(_id);
   res.status(201).json(scores);
 });
